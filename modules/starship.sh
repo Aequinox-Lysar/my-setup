f45747c465881d964bda1fd3629befb77237e86d
Aequinox Lysar <236862113+Aequinox-Lysar@users.noreply.github.com>
2025-12-05 19:02:02 +0100

    Update starship.sh

--- modules/starship.sh
+++ modules/starship.sh
@@ -1,11 +1,102 @@
+# Dieses Skript ist der versuch die installatione und configuration meines Starship Prompts zu automatisieren.
 #!/bin/bash
 echo -e "Starship wird installiert......\n"
 curl -sS https://starship.rs/install.sh | sh
 echo -e ".bashrc bekommt das Startkommando hinzugefügt....\n"
+# Hier wird Straship im Terminial aktiviert.
 cat <<EOF >>~/.bashrc
 # Starship starter
 eval "$(starship init bash)"
 EOF
+# Hier kommt die Konfiguration
 echo -e "Deine Configdatei wird angelegt.....\n"
-cat configs/starship.toml > ~/.config/starship.toml
+cat <<EOF > ~/.config/starship.toml
+format = """
+[ ](white)\
+[  ](bg:white fg:#0087AF)\
+$username\
+$hostname\
+[](bg:#769ff0 fg:white)\
+$directory\
+[](fg:#769ff0 bg:#394260)\
+$git_branch\
+$git_status\
+[](fg:#394260 bg:#212736)\
+$sudo\
+[](fg:#212736 bg:#1d2230)\
+$time\
+[ ](fg:#1d2230)\
+\n$character"""
+
+[directory]
+style = "bold fg:#e3e5e5 bg:#769ff0"
+format = "[ $path ]($style)"
+truncation_length = 8
+truncation_symbol = "…/"
+
+[sudo]
+disabled = false
+style = "fg:#ff8800 bg:#212736"
+symbol = " "
+format = "[ $symbol SUDO on]($style)" 
+
+[hostname]
+ssh_only = false
+disabled = false
+style = "bold bg:white fg:#0087AF"
+format = "[@$hostname]($style)"
+
+[character]
+success_symbol = "[ ](bold green)"
+error_symbol= "[ ](bold red)"
+
+[username]
+style_user = "bold bg:white fg:#0087AF"
+format = "[ $user]($style)"
+show_always = true
+disabled = false
+
+[directory.substitutions]
+"Dokumente" = " "
+"Downloads" = " "
+"Musik" = " "
+"Bilder" = " "
+"~" = " "
+
+[git_branch]
+symbol = ""
+style = "bg:#394260"
+format = '[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)'
+
+[git_status]
+style = "bg:#394260"
+format = '[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)'
+
+[nodejs]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[rust]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[golang]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[php]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[time]
+disabled = false
+time_format = "%R" # Hour:Minute Format
+style = "bg:#1d2230"
+format = '[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)'
+EOF
+# Abschluss
 echo -e "Starship wurde erfolgreich installiert.\n"
